42138ce3eac8cd15eb00444492a88ec70b0fe8dd
unknown <teclado1333@gmail.com>
2020-03-09 20:24:41 +0100

    agregando pantalla de inicio

--- Assets/Script/DeathZone.cs
+++ Assets/Script/DeathZone.cs
@@ -5,6 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class DeathZone : MonoBehaviour
 {
+    public AudioClip fire;
+    private AudioSource audio;
+
+
+
+
+    private void Awake()
+    {
+        audio = gameObject.GetComponent<AudioSource>();
+        audio.clip = fire;
+    }
+
+
+
     public GameObject magicFire;
     public float v =0;
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,7 +39,10 @@ public class DeathZone : MonoBehaviour
         {
             Instantiate(magicFire, transform.position +(Vector3.down*v), Quaternion.identity);
         }
-
+        if (audio != null)
+        {
+            audio.Play();
+        }
         player.SetActive(false);
         yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("Level1");
--- Assets/Script/Enemy/HealthEnemy.cs
+++ Assets/Script/Enemy/HealthEnemy.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class HealthEnemy : MonoBehaviour
 {
-    public  SpriteRenderer spriteRenderer;
 
+
+
+    public  SpriteRenderer spriteRenderer;
     public int health =100;
-    
+    public AudioClip hit;
+    private AudioSource audio;
+
+
+    // Update is called once per frame
+
+    private void Awake()
+    {
+        audio = gameObject.GetComponent<AudioSource>();
+        audio.clip = hit;
+    }
+
+
+
     public void TakeDamege(int damage)
     {
+        if (audio!=null)
+        {
+            audio.Play();
+        }
+ 
         health -= damage;
         this.spriteRenderer.color = new Color(0,150,0,50);
-
         if (health<=0)
         {
             Die();
